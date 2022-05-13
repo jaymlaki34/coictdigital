6f0ee14bc3e4f8eabf16ee6fbc81be1e9ca42b09
Angela Jovin <67833415+AngelaJovin@users.noreply.github.com>
2022-05-13 11:35:02 +0100

    Delete coictdigital directory

--- coictdigital/includes/sessionStuffs.php
+++
@@ -1,8 +0,0 @@
-<?php
-// session_start(); //already stared in headerContent.php
-if (isset($_SESSION["userData"])) {
-    $_user = $_SESSION["userData"];
-} else {
-
-    header("Location:/coictdigital/index.php");
-}
